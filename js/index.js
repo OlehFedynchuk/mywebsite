6257b27d2f3bfe64634d9d4c8abf642b11ece0fa
Oleg Fedynchuk <oleh.fedynchuk.tk.2015@lpnu.ua>
2018-11-07 14:05:48 +0000

    commit website

--- js/index.js
+++ js/index.js
@@ -1,49 +1,64 @@
-function show(state) {
+const modal = document.querySelector('.modal');
+document.querySelector('.button_big').addEventListener('click', openModal);
+document.querySelector('.close').addEventListener('click', closeModal);
 
-    document.getElementById('window').style.display = state;
-    document.getElementById('wrap').style.display = state;
+function openModal(){
+    modal.style.display = "block";
 }
 
-$('.form').find('input, textarea').on('keyup blur focus', function (e) {
-  
-  var $this = $(this),
-      label = $this.prev('label');
+function closeModal(){
+    modal.style.display = "none";
+}
 
-	  if (e.type === 'keyup') {
-			if ($this.val() === '') {
-          label.removeClass('active highlight');
-        } else {
-          label.addClass('active highlight');
-        }
-    } else if (e.type === 'blur') {
-    	if( $this.val() === '' ) {
-    		label.removeClass('active highlight'); 
-			} else {
-		    label.removeClass('highlight');   
-			}   
-    } else if (e.type === 'focus') {
-      
-      if( $this.val() === '' ) {
-    		label.removeClass('highlight'); 
-			} 
-      else if( $this.val() !== '' ) {
-		    label.addClass('highlight');
-			}
+window.addEventListener('click', function(event){
+    if(event.target === modal){
+        modal.style.display = "none";
     }
+})
+
+$('.form').find('input, textarea').on('keyup blur focus', function(e) {
+
+  var $this = $(this),
+    label = $this.prev('label');
+
+  if (e.type === 'keyup') {
+    if ($this.val() === '') {
+      label.removeClass('active highlight');
+    }
+    else {
+      label.addClass('active highlight');
+    }
+  }
+  else if (e.type === 'blur') {
+    if ($this.val() === '') {
+      label.removeClass('active highlight');
+    }
+    else {
+      label.removeClass('highlight');
+    }
+  }
+  else if (e.type === 'focus') {
+
+    if ($this.val() === '') {
+      label.removeClass('highlight');
+    }
+    else if ($this.val() !== '') {
+      label.addClass('highlight');
+    }
+  }
 
 });
 
-$('.tab a').on('click', function (e) {
-  
-  e.preventDefault();
-  
-  $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
-  
-  target = $(this).attr('href');
+var hellopreloader = document.getElementById("hellopreloader_preload");
 
-  $('.tab-content > div').not(target).hide();
-  
-  $(target).fadeIn(600);
-  
-});
+function fadeOutnojquery(el) {
+  el.style.opacity = 1;
+  var interhellopreloader = setInterval(function() {
+    el.style.opacity = el.style.opacity - 0.05;
+    if (el.style.opacity <= 0.05) {
+      clearInterval(interhellopreloader);
+      hellopreloader.style.display = "none";
+    }
+  }, 16);
+}
+window.onload = function() { setTimeout(function() { fadeOutnojquery(hellopreloader); }, 1000); };
